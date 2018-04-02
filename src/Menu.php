a0f8aaaeb9b4fc39b3070e91427cce15038a3087
Antonia Cronin <antoniajcronin@gmail.com>
2018-04-02 19:26:09 +0100

    update regexp code for vendor postcode

--- src/Menu.php
+++ src/Menu.php
@@ -1,9 +1,29 @@
 <?php
 namespace attyc\citypantry;
+
+require 'Vendor.php'
 class Menu    {
   public
-    function vendors()
-    {
-      return [];
-    }
+
+  function parseFile($file){
+    $file = new SplFileObject("vendors.txt");
+    $vendor = new Vendor();
+
+    // Loop until we reach the end of the file.
+    while (!$file->eof()) {
+      $line = $file->fgets();
+
+      if($line != "\n"){
+        //if line ends with max covers = vendor
+        if(preg_match("/[a-zA-Z]{1,2}([0-9]{1,2}|[0-9][a-zA-Z])\s*[0-9][a-zA-Z]{2}/", $line)){
+          $vendor->createVendor($line);
+        }
+        else if(preg_match("/\d\dh/", $line)){
+          echo 'item' . $line;
+          //$items->createItem($line, $vendor);
+        };
+      };
+    };
+  }
+
 }
